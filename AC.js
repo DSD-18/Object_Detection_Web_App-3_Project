6df193eb56dfb503cd0439f0947f5490e54ca406
DSD-18 <72644829+DSD-18@users.noreply.github.com>
2021-05-29 15:38:53 +0530

    Add files via upload

---
+++ AC.js
@@ -0,0 +1,40 @@
+status = "";
+objects = [];
+img = "";
+
+function preload(){
+    img = loadImage('TV.jpg');
+  }
+  
+function setup() {
+    canvas = createCanvas(540, 320);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+  
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+  function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+  if(status != "")
+  {
+    for (var i = 0; i < objects.length; i++)
+    {
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
